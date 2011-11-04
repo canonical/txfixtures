ac64e72757752fcb9587bfb3cfb6968c6e06ed64
Martin Pool <mbp@canonical.com>
2011-11-04 13:07:28 +1100

    Add distutils setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""distutils metadata/installer for txfixtures"""
+
+
+from distutils.core import setup
+
+
+setup(
+    name='txfixtures',
+    maintainer='Martin Pool',
+    maintainer_email='mbp@canonical.com',
+    url='https://launchpad.net/txfixtures',
+    description=('Treat Twisted applications as Python test fixtures'),
+    version='0.1',
+    classifiers=["License :: OSI Approved :: GNU GPL v3"],
+    packages=['txfixtures'],
+    )
