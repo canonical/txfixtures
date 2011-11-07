ac87adf28de122cabb82fd811b380c5f0951a47f
Martin Pool <mbp@canonical.com>
2011-11-07 17:40:43 +1100

    Add more pypi metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,21 @@
 """distutils metadata/installer for txfixtures"""
 
 
+import os.path
 from distutils.core import setup
+
+import txfixtures
+
+
+def get_version():
+    return '.'.join(
+        str(component) for component in txfixtures.__version__[0:3])
+
+
+def get_long_description():
+    readme_path = os.path.join(
+        os.path.dirname(__file__), 'README')
+    return open(readme_path).read()
 
 
 setup(
@@ -12,7 +26,8 @@ setup(
     maintainer_email='mbp@canonical.com',
     url='https://launchpad.net/txfixtures',
     description=('Treat Twisted applications as Python test fixtures'),
-    version='0.1.2',
+    long_description=get_long_description(),
+    version=get_version(),
     classifiers=["License :: OSI Approved :: GNU General Public License (GPL)"],
     packages=['txfixtures'],
     requires=[
